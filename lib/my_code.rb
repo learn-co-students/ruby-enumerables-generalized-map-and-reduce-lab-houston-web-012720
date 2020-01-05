3651c892113866d4fda2091eb736ce5d9732ac21
William C Rutland <william.prometheus@gmail.com>
2020-01-05 16:47:21 +0000

    Done.

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,28 @@
-# Your Code Here
+
+def map(array)
+  new_array = []
+  i = 0
+  while i < array.length
+    new_array.push(yield(array[i]))
+    i +=1
+  end
+  new_array
+end
+
+map([1, 2, 3, -9]) { |n| n * -1 }
+
+def reduce(array, nv = nil)
+  if nv
+    sum = nv
+    i = 0
+  else
+    sum = array[0]
+    i = 1
+  end
+  
+  while i < array.length
+    sum = yield(sum, array[i])
+    i += 1
+  end
+  sum
+end
