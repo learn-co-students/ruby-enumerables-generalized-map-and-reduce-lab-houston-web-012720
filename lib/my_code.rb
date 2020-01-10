17e6736781d32d8072a1d79b20996303c2c663cf
Stephen Williams <stephen.r.williams.87@gmail.com>
2020-01-10 07:45:35 +0000

    Done.

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,26 @@
-# Your Code Here
+def map (data)
+  i = 0
+  new_data = []
+  while i < data.length do
+    new_data << yield(data[i])
+    i += 1
+  end
+  new_data
+end
+
+def reduce (data, starting_point = nil)
+  if starting_point
+    new_data = starting_point
+    i = 0
+# I missed the else section from here...
+  else
+    new_data = data[0]
+    i = 1
+# ...to here. And the "if" conditional, of course.
+  end
+  while i < data.length do
+    new_data = yield(new_data, data[i])
+    i += 1
+  end
+  new_data
+end
